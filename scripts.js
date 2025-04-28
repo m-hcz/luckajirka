b252b9f04ca608580048bbb2aa7c2a4ebec7a0a6
Michal Heczko <michal.heczko@cadconsulting.cz>
2025-04-28 18:44:09 +0200

    countdown

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 // Set the target date
 const targetDate = new Date("2025-09-06T13:00:00").getTime();
+//const targetDate = new Date("2025-04-28T18:42:30").getTime();
 
 // Update the countdown every second
 const countdown = setInterval(function() {
   const now = new Date().getTime();
   const distance = targetDate - now;
+
+  // If the countdown is finished
+  if (distance <= 0) {
+    clearInterval(countdown);
+    return;
+  }
 
   // Time calculations
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -18,11 +25,7 @@ const countdown = setInterval(function() {
   document.getElementById("minutes").innerText = formatTime(minutes);
   document.getElementById("seconds").innerText = formatTime(seconds);
 
-  // If the countdown is finished
-  if (distance < 0) {
-    clearInterval(countdown);
-    document.getElementById("countdown").innerHTML = "";
-  }
+  
 }, 1000);
 
 function formatTime(unit) {
